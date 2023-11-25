e78b4aebf360fdbfc06d50a50ed487c1563d70a9
Cakeqb5 <joelmbparker@gmail.com>
2023-11-24 19:43:40 -0800

    seemed to brought tests back to where they were mostly, have a look

--- com.thelocalmarketplace.hardware.test/src/testingUtilities/CardPayment.java
+++ com.thelocalmarketplace.hardware.test/src/testingUtilities/CardPayment.java
@@ -6,10 +6,25 @@ import com.jjjwelectronics.card.Card;
 import com.thelocalmarketplace.hardware.external.CardIssuer;
 
 public interface CardPayment {
+
+	boolean tapEnabled=true;
+	boolean tapDisabled=false;
+	
+	boolean hasChip=true;
+	boolean noChip=false;
+	
+	/*
+	 * card issuer and card might be related, 
+	 */
 	CardIssuer creditcard= new CardIssuer("credit", 500);
 	CardIssuer debitcard= new CardIssuer("debit", 500);
-
-	Card otherCreditCard = new Card("credit", "555", "Donkey Kong", "312");
-	Card otherDebitCard = new Card("debit", "555", "Donkey Kong", "312");
+	
+	
+	/*
+	 * copy the initializing parameters to add card data so a card can pay.
+	 */
+	
+	Card otherCreditCard = new Card("credit", "555", "Donkey Kong", "312", "911", tapEnabled, hasChip);
+	Card otherDebitCard = new Card("credit", "555", "Donkey Kong", "312", "911", tapEnabled, hasChip);
 	
 }
