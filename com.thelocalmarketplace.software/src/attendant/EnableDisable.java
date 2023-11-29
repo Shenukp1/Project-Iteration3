8190d0e5601ca90c2b5bc7eeaabf10423793253e
sinasalahshour <sinasalahshr@gmail.com>
2023-11-28 20:50:43 -0700

    Created the logic for enabling and disabling components by attendant.

--- com.thelocalmarketplace.software/src/attendant/EnableDisable.java
+++ com.thelocalmarketplace.software/src/attendant/EnableDisable.java
@@ -1,21 +1,24 @@
 package attendant;
 
+import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
+
 public class EnableDisable {
 
     private StationStatus status;
-    private boolean isSessionActive;
     private String authorizedPassword; // Password for authorization
+    private AbstractSelfCheckoutStation station;
 
     enum StationStatus {
         ENABLED,
         DISABLED,
         OUT_OF_ORDER // When maintenance is required
     }
+
     // Constructor
-    public EnableDisable(String authorizedPassword) {
+    public EnableDisable(String authorizedPassword, AbstractSelfCheckoutStation station) {
         this.status = StationStatus.ENABLED; // Default status
-        this.isSessionActive = false;
         this.authorizedPassword = authorizedPassword; // Set the authorized password
+        this.station = station; // Initialize the station reference
     }
 
     /**
@@ -24,12 +27,17 @@ public class EnableDisable {
      * @return boolean True if operation was successful, false otherwise.
      */
     public boolean enableStation(String password) {
-        if (isAuthorized(password) && status == StationStatus.DISABLED) {
-            status = StationStatus.ENABLED;
-            // Logic to be implemented
-            return true;
+        try {
+            if (isAuthorized(password) && status == StationStatus.DISABLED) {
+                enableHardwareComponents();
+                status = StationStatus.ENABLED;
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            System.err.println("Failed to enable station: " + e.getMessage());
+            return false;
         }
-        return false;
     }
 
     /**
@@ -38,12 +46,53 @@ public class EnableDisable {
      * @return boolean True if operation was successful, false otherwise.
      */
     public boolean disableStation(String password) {
-        if (isAuthorized(password) && !isSessionActive) {
-            status = StationStatus.DISABLED;
-            // Logic to be implemented
-            return true;
+        try {
+            if (isAuthorized(password) && status == StationStatus.ENABLED) {
+                disableHardwareComponents();
+                status = StationStatus.DISABLED;
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            System.err.println("Failed to disable station: " + e.getMessage());
+            return false;
         }
-        return false;
+    }
+
+    /**
+     * Enable hardware components of the station.
+     * @throws Exception If there is a failure in enabling hardware components.
+     */
+    private void enableHardwareComponents() throws Exception {
+        station.banknoteInput.enable();
+        station.coinSlot.enable();
+        station.cardReader.enable();
+        station.screen.enable();
+        station.mainScanner.enable();
+        station.handheldScanner.enable();
+        station.printer.enable();
+        station.scanningArea.enable();
+        station.baggingArea.enable();
+        station.screen.enable();
+        station.reusableBagDispenser.enable();
+    }
+
+    /**
+     * Disable hardware components of the station.
+     * @throws Exception If there is a failure in disabling hardware components.
+     */
+    private void disableHardwareComponents() throws Exception {
+        station.banknoteInput.disable();
+        station.coinSlot.disable();
+        station.cardReader.disable();
+        station.screen.disable();
+        station.mainScanner.disable();
+        station.handheldScanner.disable();
+        station.printer.disable();
+        station.scanningArea.disable();
+        station.baggingArea.disable();
+        station.screen.disable();
+        station.reusableBagDispenser.disable();
     }
 
     /**
