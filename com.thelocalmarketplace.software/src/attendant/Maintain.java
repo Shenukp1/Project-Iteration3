e94e3fb00c4a7c710063bb35b56f2d14af60c53e
givenn19 <lesiepp@gmail.com>
2023-11-28 22:38:06 -0700

    Maintain class it.1 complete

---
+++ com.thelocalmarketplace.software/src/attendant/Maintain.java
@@ -0,0 +1,50 @@
+package attendant;
+
+import com.jjjwelectronics.OverloadedDevice;
+import com.jjjwelectronics.printer.IReceiptPrinter;
+import com.tdc.CashOverloadException;
+import com.tdc.banknote.Banknote;
+import com.tdc.banknote.BanknoteStorageUnit;
+import com.tdc.coin.Coin;
+import com.tdc.coin.CoinStorageUnit;
+import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
+import ca.ucalgary.seng300.simulation.SimulationException;
+
+public class Maintain {
+	private IReceiptPrinter printer;
+	private BanknoteStorageUnit banknoteStorage;
+	private CoinStorageUnit coinStorage;
+	
+	
+	public Maintain(AbstractSelfCheckoutStation station) {
+		this.printer = station.printer;
+		this.banknoteStorage = station.banknoteStorage;
+		this.coinStorage = station.coinStorage;
+	}
+	
+	// Attendant adds ink
+	public void maintainInk(int quantity) throws OverloadedDevice {
+		printer.addInk(quantity); // AbstractReceiptPrinter. Announces "inkAdded" event. Requires power.
+		
+	}
+	
+	
+	// Attendant adds paper 
+	public void maintainPaper(int quantity) throws OverloadedDevice {
+		printer.addPaper(quantity); // AbstractReceiptPrinter. Announces "paperAdded" event. Requires power.
+		
+	}
+	
+	// Attendant adds bank notes
+	public void maintainBanknotes(Banknote... banknotes) throws SimulationException, CashOverloadException {
+		banknoteStorage.load(banknotes);// BanknoteStorageUnit. Announces "banknotesLoaded" event. Disabling has no effect. Requires power.
+
+	}
+	
+	// Attendant adds coins
+	public void maintainCoins(Coin... coins) throws SimulationException, CashOverloadException {
+		coinStorage.load(coins);// CoinStorageUnit. Announces "coinsLoaded" event. Disabling has no effect. Requires power.
+	}
+	
+	
+}
--- com.thelocalmarketplace.software/src/attendant/maintain.java
+++
@@ -1,5 +0,0 @@
-package attendant;
-
-public class maintain {
-
-}
