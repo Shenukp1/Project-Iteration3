1fcfaf57dcb3420306ffc450f2c80afa1db5b5e4
Jvery11 <jvery11@gmail.com>
2023-12-03 00:12:12 -0700

    Rewrote someTests

--- com.thelocalmarketplace.hardware.test/src/com/thelocalmarketplace/hardware/test/PredictIssueTest.java
+++ com.thelocalmarketplace.hardware.test/src/com/thelocalmarketplace/hardware/test/PredictIssueTest.java
@@ -28,13 +28,8 @@ import control.SessionController;
 import powerutility.PowerGrid;
 
 public class PredictIssueTest {
-
 	SelfCheckoutStationBronze bronze;
 	SelfCheckoutLogic logicBronze;
-
-	Banknote banknote;
-	Coin coin;
-	BigDecimal denoms;
 
 	/*
 	 * Since receipt printer is bronze for all stations and dispensers + storage are
@@ -49,179 +44,163 @@ public class PredictIssueTest {
 		bronze.plugIn(PowerGrid.instance());
 		bronze.turnOn();
 		logicBronze = SelfCheckoutLogic.installOn(bronze);
-		banknote = new Banknote(Currency.getInstance(Locale.CANADA), new BigDecimal(10.00));
-		coin = new Coin(Currency.getInstance(Locale.CANADA), new BigDecimal(1.00));
 	}
 
 	// Fill both dispensers and storage units almost to max capacity
 	// Bronze machine
 	public void fillMachine() throws SimulationException, CashOverloadException {
-		BigDecimal denoms = this.bronze.getCoinDenominations().get(0);
 		Banknote banknote = new Banknote(Currency.getInstance(Locale.CANADA), new BigDecimal(10.00));
 		Coin coin = new Coin(Currency.getInstance(Locale.CANADA), new BigDecimal(1.00));
-		ICoinDispenser coinDispenser = this.bronze.getCoinDispensers().get(denoms);
-		IBanknoteDispenser banknoteDispenser = this.bronze.getBanknoteDispensers().get(denoms);
 		for (int i = 0; i < bronze.getCoinStorage().getCapacity() - 2; i++) {
 			this.bronze.getCoinStorage().load(coin);
 		}
-		for (int i = 0; i < coinDispenser.getCapacity() - 2; i++) {
-			coinDispenser.load(coin);
+		for (int i = 0; i < bronze.getCoinDenominations().size(); i++) {
+			BigDecimal coinDenoms = this.bronze.getCoinDenominations().get(i);
+			ICoinDispenser coinDispenser = this.bronze.getCoinDispensers().get(coinDenoms);
+			for (int j = 0; j < coinDispenser.getCapacity() - 2; j++) {
+				coinDispenser.load(coin);
+			}
 		}
 		for (int i = 0; i < bronze.getBanknoteStorage().getCapacity() - 2; i++) {
 			this.bronze.getBanknoteStorage().load(banknote);
 		}
-		for (int i = 0; i < banknoteDispenser.getCapacity() - 2; i++) {
-			banknoteDispenser.load(banknote);
+		for (int j = 0; j < bronze.getBanknoteDenominations().length; j++) {
+			BigDecimal noteDenoms = this.bronze.getBanknoteDenominations()[j];
+			IBanknoteDispenser banknoteDispenser = this.bronze.getBanknoteDispensers().get(noteDenoms);
+			for (int i = 0; i < banknoteDispenser.getCapacity() - 2; i++) {
+				banknoteDispenser.load(banknote);
+			}
 		}
 	}
 
-//	/*
-//	 * Individual method tests
-//	 */
-//	// Testing to see if banknotes are almost full in storage
-//	// empty machine
-//	@Test
-//	public void testNotAlmostFullBanknote() {
-//		logicBronze.predictController.predictFullBanknote();
-//		// Storage is empty so expected = false
-//		assertFalse(logicBronze.predictController.banknoteAlmostFull());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
-//	}
-//
-//	// Test to see if banknotes are almost full in storage
-//	// filled up machine
-//	@Test
-//	public void testAlmostFullBanknote() throws SimulationException, CashOverloadException {
-//		fillMachine();
-//		logicBronze.predictController.predictFullBanknote();
-//		// Storage is almost full expected = true
-//		// Should be one issue added to the attendant GUI
-//		assertTrue(logicBronze.predictController.banknoteAlmostFull());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 1);
-//	}
-//
-//	// Testing to see if the banknote dispenser is almost empty
-//	// empty machine
-//	@Test
-//	public void testAlmostEmptyBanknote() {
-//		logicBronze.predictController.predictLowBanknote();
-//		// Dispensers are empty expected = true
-//		// Should have one issues sent to attendant GUI
-//		assertTrue(logicBronze.predictController.banknoteAlmostEmpty());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 1);
-//	}
-//
-//	// Testing to see if banknote dispenser is almost empty
-//	// full machine
-//	@Test
-//	public void testNotAlmostEmptyBanknote() throws SimulationException, CashOverloadException {
-//		fillMachine();
-//		logicBronze.predictController.predictLowBanknote();
-//		// Dispensers are full expected = false
-//		// No issues should be sent to the attendant
-//		assertFalse(logicBronze.predictController.banknoteAlmostEmpty());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
-//	}
-//
-//	// Testing to see if coins are almost full in storage
-//	// empty machine
-//	@Test
-//	public void testNotAlmostCoin() {
-//		logicBronze.predictController.predictFullCoin();
-//		// Storage is empty so expected = false
-//		assertFalse(logicBronze.predictController.coinAlmostFull());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
-//	}
-//
-//	 // Test to see if coins are almost full in storage
-//	 // filled up machine
-//	@Test
-//	public void testAlmostFullCoin() throws SimulationException, CashOverloadException {
-//		fillMachine();
-//		logicBronze.predictController.predictFullCoin();
-//		// Storage is almost full expected = true
-//		// Should be one issue added to the attendant GUI
-//		assertTrue(logicBronze.predictController.coinAlmostFull());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 1);
-//	}
-//
-//	// Testing to see if the coin dispensers are almost empty
-//	// empty machine
-//	@Test
-//	public void testAlmostEmptyCoin() {
-//		logicBronze.predictController.predictLowCoin();
-//		// Dispensers are empty expected = true
-//		// Should have one issues sent to attendant GUI
-//		assertTrue(logicBronze.predictController.coinAlmostEmpty());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 1);
-//	}
-//
-//	// Testing to see if coin dispensers are almost empty
-//	// full machine
-//	@Test
-//	public void testNotAlmostEmptyCoin() throws SimulationException, CashOverloadException {
-//		fillMachine();
-//		logicBronze.predictController.predictLowCoin();
-//		// Dispensers are full expected = false
-//		// No issues should be sent to the attendant
-//		assertFalse(logicBronze.predictController.coinAlmostEmpty());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
-//	}
-//
-//	// Testing to see if issues are sent to attendant
-//	// When printer is full on paper
-//	@Test
-//	public void testFullPaper() throws OverloadedDevice {
-//		bronze.getPrinter().addPaper(1000);
-//		logicBronze.predictController.predictLowPaper();
-//		assertFalse(logicBronze.predictController.paperAlmostEmpty());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
-//	}
+	/*
+	 * Individual method tests
+	 */
+	// Testing to see if banknotes are almost full in storage
+	// empty machine
+	@Test
+	public void testNotAlmostFullBanknote() {
+		// Storage is empty so expected = false
+		assertFalse(logicBronze.predictController.banknoteAlmostFull());
+	}
+
+	// Test to see if banknotes are almost full in storage
+	// filled up machine
+	@Test
+	public void testAlmostFullBanknote() throws SimulationException, CashOverloadException {
+		fillMachine();
+		logicBronze.predictController.predictFullBanknote();
+		// Storage is almost full expected = true
+		// Should be one issue added to the attendant GUI
+		assertTrue(logicBronze.predictController.banknoteAlmostFull());
+	}
+
+	// Testing to see if the banknote dispenser is almost empty
+	// empty machine
+	@Test
+	public void testAlmostEmptyBanknote() {
+		// Dispensers are empty expected = true
+		// Should have one issues sent to attendant GUI
+		assertTrue(logicBronze.predictController.banknoteAlmostEmpty());
+	}
+
+	// Testing to see if banknote dispenser is almost empty
+	// full machine
+	@Test
+	public void testNotAlmostEmptyBanknote() throws SimulationException, CashOverloadException {
+		fillMachine();
+		logicBronze.predictController.predictLowBanknote();
+		// Dispensers are full expected = false
+		// No issues should be sent to the attendant
+		assertFalse(logicBronze.predictController.banknoteAlmostEmpty());
+	}
+
+	// Testing to see if coins are almost full in storage
+	// empty machine
+	@Test
+	public void testNotAlmostCoin() {
+		// Storage is empty so expected = false
+		assertFalse(logicBronze.predictController.coinAlmostFull());
+	}
+
+	// Test to see if coins are almost full in storage
+	// filled up machine
+	@Test
+	public void testAlmostFullCoin() throws SimulationException, CashOverloadException {
+		fillMachine();
+		logicBronze.predictController.predictFullCoin();
+		// Storage is almost full expected = true
+		// Should be one issue added to the attendant GUI
+		assertTrue(logicBronze.predictController.coinAlmostFull());
+	}
+
+	// Testing to see if the coin dispensers are almost empty
+	// empty machine
+	@Test
+	public void testAlmostEmptyCoin() {
+		// Dispensers are empty expected = true
+		// Should have one issues sent to attendant GUI
+		assertTrue(logicBronze.predictController.coinAlmostEmpty());
+	}
+
+	// Testing to see if coin dispensers are almost empty
+	// full machine
+	@Test
+	public void testNotAlmostEmptyCoin() throws SimulationException, CashOverloadException {
+		fillMachine();
+		logicBronze.predictController.predictFullCoin();
+		// Dispensers are full expected = false
+		// No issues should be sent to the attendant
+		assertFalse(logicBronze.predictController.coinAlmostEmpty());
+	}
+
+	// Testing to see if issues are sent to attendant
+	// When printer is full on paper
+	@Test
+	public void testFullPaper() throws OverloadedDevice {
+		bronze.getPrinter().addPaper(1024);
+		assertFalse(logicBronze.predictController.paperAlmostEmpty());
+	}
 
 	// Testing to see if issues are sent to attendant
 	// When printer is low on paper
 	@Test
-	public void testLowPaper() throws OverloadedDevice, EmptyDevice, SimulationException, CashOverloadException {
-		fillMachine();
+	public void testLowPaper() throws OverloadedDevice, EmptyDevice {
 		bronze.getPrinter().addPaper(1024);
 		bronze.getPrinter().addInk(2000);
 		for (int i = 0; i < 1020; i++) {
 			bronze.getPrinter().print('c');
 			bronze.getPrinter().print('\n');
 		}
-		logicBronze.predictController.predictLowPaper();
 		assertTrue(logicBronze.predictController.paperAlmostEmpty());
+	}
+
+	// Testing to see if issues are sent to attendant
+	// When printer is full on paper
+	@Test
+	public void testFullInk() throws OverloadedDevice {
+//		bronze.getPrinter().addInk(1000);
+//		logicBronze.predictController.predictLowInk();
+//		assertFalse(logicBronze.predictController.inkAlmostEmpty());
+//		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
+	}
+
+	// Testing to see if issues are sent to attendant
+	// When printer is low on paper
+	@Test
+	public void testLowInk() throws OverloadedDevice, EmptyDevice {
+		// Would have to print over a million characters to test if ink is low?
+		// Maybe have ink low based off how many receipts are printed
+		bronze.getPrinter().addPaper(1024);
+		bronze.getPrinter().addInk(1048576);
+		for (int i = 0; i < 1020; i++) {
+			for (int j = 0; j < 60; j++) {
+				bronze.getPrinter().print('c');
+			}
+			bronze.getPrinter().print('\n');
+		}
+		logicBronze.predictController.predictLowInk();
+		assertTrue(logicBronze.predictController.inkAlmostEmpty());
 		assertEquals(logicBronze.predictController.numberOfIssues(), 1);
 	}
-//
-//	// Testing to see if issues are sent to attendant
-//	// When printer is full on paper
-//	@Test
-//	public void testFullInk() throws OverloadedDevice {
-////		bronze.getPrinter().addInk(1000);
-////		logicBronze.predictController.predictLowInk();
-////		assertFalse(logicBronze.predictController.inkAlmostEmpty());
-////		assertEquals(logicBronze.predictController.numberOfIssues(), 0);
-//	}
-//
-//	// Testing to see if issues are sent to attendant
-//	// When printer is low on paper
-//	@Test
-//	public void testLowInk() throws OverloadedDevice, EmptyDevice {
-//		// Would have to print over a million characters to test if ink is low?
-//		// Maybe have ink low based off how many receipts are printed 
-//		bronze.getPrinter().addPaper(1024);
-//		bronze.getPrinter().addInk(1048576);
-//
-//		for (int i = 0; i < 1020; i++) {
-//			for (int j = 0; j < 60; j++) {
-//				bronze.getPrinter().print('c');
-//			}
-//			bronze.getPrinter().print('\n');
-//		}
-//
-//		logicBronze.predictController.predictLowInk();
-//		assertTrue(logicBronze.predictController.inkAlmostEmpty());
-//		assertEquals(logicBronze.predictController.numberOfIssues(), 1);
-//	}
 }
