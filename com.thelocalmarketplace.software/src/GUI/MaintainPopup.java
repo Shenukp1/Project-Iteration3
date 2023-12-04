fddc4e87e9df3a7e2916686c7d2ceae6b7821a32
Kian <98991063+givenn19@users.noreply.github.com>
2023-12-03 22:37:25 -0700

    doc

--- com.thelocalmarketplace.software/src/GUI/MaintainPopup.java
+++ com.thelocalmarketplace.software/src/GUI/MaintainPopup.java
@@ -13,9 +13,11 @@ import control.SelfCheckoutLogic;
  */
 public class MaintainPopup {
     
-	
-    // frame should say "Maintenance required at customer station." followed by a maintainance message.
-	
+	/**
+	 * Method takes a maintenance message from Maintain and then creates a pop up.
+	 * Pop up should say "Maintenance required at customer station." followed by a maintainance message.
+	 * @param maintenanceRequired
+	 */
     public static void showMaintenancePopup(Maintain maintenanceRequired) {
     	
     	String maintainMessage = maintenanceRequired.getMessage(); // TODO update from Maintain class
@@ -23,7 +25,7 @@ public class MaintainPopup {
         String message = "Maintenance required at customer station." + ".\n"
                 + maintainMessage;
 
-        // Show a pop-up dialog with an OK button (default frame used?)
+        // Show a pop-up dialog with an OK button (default frame used)
         JOptionPane.showMessageDialog(null, message, "Maintenance Alert", JOptionPane.INFORMATION_MESSAGE);
     }
 }
