45bb9e59f171fec48d450e0cd1775516d10dd3e6
ishita-udasi <124753887+ishita-udasi@users.noreply.github.com>
2023-12-04 22:50:32 -0700

    Update PurchaseBagsTest.java

--- com.thelocalmarketplace.hardware.test/src/com/thelocalmarketplace/hardware/test/PurchaseBagsTest.java
+++ com.thelocalmarketplace.hardware.test/src/com/thelocalmarketplace/hardware/test/PurchaseBagsTest.java
@@ -1,4 +1,26 @@
-// Kear Sang Heng 30087289
+/*Group P3-6***
+Andy Tang 10139121
+Ayman Inayatali Momin 30192494
+Darpal Patel 30088795
+Dylan Dizon 30173525
+Ellen Bowie 30191922
+Emil Huseynov 30171501
+Ishita Udasi 30170034
+Jason Very 30222040
+Jesse Leinan 00335214
+Joel Parker 30021079
+Kear Sang Heng 30087289
+Khadeeja Abbas 30180776
+Kian Sieppert 30134666
+Michelle Le 30145965
+Raja Muhammed Omar 30159575
+Sean Gilluley 30143052
+Shenuk Perera 30086618
+Simrat Virk 30000516
+Sina Salahshour 30177165
+Tristan Van Decker 30160634
+Usharab Khan 30157960
+YiPing Zhang 30127823*/
 package com.thelocalmarketplace.hardware.test;
 import static org.junit.Assert.*;
 
